b494f86b069b7637da26ce16e97ac9c19d0808f3
David mattei <d.mattei89@gmail.com>
2025-02-04 15:15:13 +0100

    feat(common/asset): vite php dev server (#1182)

--- src/Helper/Asset/AssetVersionStrategy.php
+++ src/Helper/Asset/AssetVersionStrategy.php
@@ -4,25 +4,16 @@ declare(strict_types=1);
 
 namespace EMS\AdminUIBundle\Helper\Asset;
 
-use EMS\Helpers\Standard\Json;
-use EMS\Helpers\Standard\Type;
-use Symfony\Component\Asset\Exception\RuntimeException;
+use EMS\CommonBundle\Common\Asset\ViteService;
 use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Config\FileLocator;
 
-final class AssetVersionStrategy implements VersionStrategyInterface
+final readonly class AssetVersionStrategy implements VersionStrategyInterface
 {
-    /**
-     * @var array<string, array{file: string, name: string, css: ?string[]}>
-     */
-    private array $manifestData;
-
     public function __construct(
-        private readonly FileLocator $fileLocator,
-        private readonly RequestStack $requestStack,
-        private readonly DevServer $devServer,
-        private readonly string $basePath = 'bundles/emsadminui/',
+        private FileLocator $fileLocator,
+        private ViteService $viteService,
+        private string $basePath = 'bundles/emsadminui/',
     ) {
     }
 
@@ -35,33 +26,12 @@ final class AssetVersionStrategy implements VersionStrategyInterface
     #[\Override]
     public function applyVersion(string $path): string
     {
-        return $this->getManifestPath($path) ?: $path;
-    }
+        $this->viteService->loadManifestFromDirectory(
+            directory: $this->fileLocator->locate('@EMSAdminUIBundle/public')
+        );
 
-    private function getManifestPath(string $path): string
-    {
-        $request = $this->requestStack->getCurrentRequest();
+        $devPath = $this->viteService->devPath($path);
 
-        if ($this->devServer->isRunning() && !\str_ends_with($path, '.css')) {
-            return $this->devServer->getPath($path);
-        }
-
-        if (!isset($this->manifestData)) {
-            $manifestPath = $this->fileLocator->locate('@EMSAdminUIBundle/public/.vite/manifest.json');
-            if (!\is_file($manifestPath)) {
-                throw new RuntimeException(\sprintf('Asset manifest file "%s" does not exist. Did you forget to build the assets with npm or yarn?', $manifestPath));
-            }
-            $this->manifestData = Json::decode(Type::string(\file_get_contents($manifestPath)));
-        }
-
-        if (\preg_match('/(?<path>.*\.(js|ts|cjs))(\.(?<index>[0-9]+))?\.css$/', $path, $matches) > 0 && isset($this->manifestData[$matches['path']]['css'][$matches['index'] ?? 0])) {
-            return $this->basePath.$this->manifestData[$matches['path']]['css'][$matches['index'] ?? 0];
-        }
-
-        if (isset($this->manifestData[$path]['file'])) {
-            return $this->basePath.$this->manifestData[$path]['file'];
-        }
-
-        return $this->basePath.$path;
+        return $devPath ?? $this->basePath.$this->viteService->path($path);
     }
 }
